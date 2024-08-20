7453183337484997d946e7793033fb7793235fa2
Sathya Narayanan Balamurugan <Sathya.vrv@gmail.com>
2024-08-20 02:24:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def calculate_kpis(df):
 
 # Load your pre-trained model
 def load_model():
-    return joblib.load("lgb_model_fit_aug18.pkl")
+    return joblib.load("model_fold_1.pkl")
 
 def calculate_fibonacci_levels(df):
     fib_ratios = [0.382, 0.5, 0.618, 0.786, 1.5, 1.618]
@@ -435,11 +435,11 @@ else:
     model = load_model()
 
     prediction = model.predict(data_df)
-    #prediction_proba = model.predict_proba(data_df)
+    prediction_proba = model.predict_proba(data_df)
 
     st.subheader("Model Prediction")
-    #st.write("Prediction:", prediction[0])
-    st.write("Prediction Probability:", prediction)
+    st.write("Prediction:", prediction)
+    st.write("Prediction Probability:", prediction_proba)
 
     st.markdown("---")
     st.write("**Note:** Ensure that the retrieved data is correct and there are no missing values before proceeding with model prediction.")
